4092b2e5b467779b13eae72b207568993c3f5db4
bigabdoul <kababdoul@hotmail.com>
2018-10-18 07:23:24 +0000

    refactor PaginationModel classes

--- src/Models/PaginationModel.cs
+++ src/Models/PaginationModel.cs
@@ -1,12 +1,21 @@
 namespace HelpDeskCore.Models
 {
-  public class PaginationModel
+  public class PaginationModelBase
   {
     public int? Page { get; set; }
     public int? Size { get; set; }
     public int? SortBy { get; set; }
     public string Query { get; set; }
     public string UserId { get; set; }
+  }
+
+  public class PaginationModel : PaginationModelBase
+  {
     public Data.Entities.UserFilter? Column { get; set; }
+  }
+
+  public class TicketPaginationModel : PaginationModelBase
+  {
+    public Data.Entities.TicketFilter? Column { get; set; }
   }
 }
