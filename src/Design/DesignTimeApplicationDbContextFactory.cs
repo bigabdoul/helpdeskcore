521cfb4e8ad389af608c9118dae59df1c55284ca
bigabdoul <kababdoul@hotmail.com>
2019-07-14 20:00:09 +0000

    add design time DbContext factory

---
+++ src/Design/DesignTimeApplicationDbContextFactory.cs
@@ -0,0 +1,31 @@
+using HelpDeskCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+
+namespace HelpDeskCore.Design
+{
+  public class DesignTimeApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+  {
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddJsonFile("appsettings.development.json");
+
+      var connectionString = builder.Build().GetConnectionString("DefaultConnection");
+
+      if (!string.IsNullOrWhiteSpace(connectionString))
+      {
+        var context = new DbContextOptionsBuilder<ApplicationDbContext>()
+          .UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(context.Options);
+      }
+
+      throw new InvalidProgramException("Cannot load the default connection string for design time DbContext.");
+    }
+  }
+}
