44bd7abb72283ffb1eda306354da855ffa115e20
bigabdoul <kababdoul@hotmail.com>
2019-09-05 08:43:59 +0000

    remove unused using statement

--- src/Design/DesignTimeApplicationDbContextFactory.cs
+++ src/Design/DesignTimeApplicationDbContextFactory.cs
@@ -2,30 +2,29 @@ using HelpDeskCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using System;
 
 namespace HelpDeskCore.Design
 {
-  public class DesignTimeApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-  {
-    public ApplicationDbContext CreateDbContext(string[] args)
+    public class DesignTimeApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
-      var builder = new ConfigurationBuilder()
-        .SetBasePath(Environment.CurrentDirectory)
-        .AddJsonFile("appsettings.development.json");
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Environment.CurrentDirectory)
+              .AddJsonFile("appsettings.development.json");
 
-      var connectionString = builder.Build().GetConnectionString("DefaultConnection");
+            var connectionString = builder.Build().GetConnectionString("DefaultConnection");
 
-      if (!string.IsNullOrWhiteSpace(connectionString))
-      {
-        var context = new DbContextOptionsBuilder<ApplicationDbContext>()
-          .UseSqlServer(connectionString);
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                var context = new DbContextOptionsBuilder<ApplicationDbContext>()
+                  .UseSqlServer(connectionString);
 
-        return new ApplicationDbContext(context.Options);
-      }
+                return new ApplicationDbContext(context.Options);
+            }
 
-      throw new InvalidProgramException("Cannot load the default connection string for design time DbContext.");
+            throw new InvalidProgramException("Cannot load the default connection string for design time DbContext.");
+        }
     }
-  }
 }
